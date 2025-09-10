bbc1d9f6ddd346c161500c92010b9a12bf6a2fbc
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2025-09-09 23:17:31 -0400

    Add missing field to employeeDTO

--- backend/Models/Employee.cs
+++ backend/Models/Employee.cs
@@ -8,24 +8,26 @@ public class Employee
     public long Id { get; set; }
 
     [Required]
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = string.Empty;
 
     [Required]
-    public string LastName { get; set; }
+    public string LastName { get; set; } = string.Empty;
 
     // TODO: Add regular expression validator [RegularExpression(@"")]
     public string PhoneNumber { get; set; } = string.Empty;
 
     [Required]
-    public Campus Campus { get; set; }
+    public Campus Campus { get; set; } = null!;
 
     [Required]
-    public Department Department { get; set; }
+    public Department Department { get; set; } = null!;
 
     public void UpdateWithDTO(EmployeeDTO employeeDTO)
     {
         FirstName = employeeDTO.FirstName;
         LastName = employeeDTO.LastName;
         PhoneNumber = employeeDTO.PhoneNumber;
+        Campus = employeeDTO.Campus;
+        Department = employeeDTO.Department;
     }
 }
--- backend/Models/EmployeeDTO.cs
+++ backend/Models/EmployeeDTO.cs
@@ -8,13 +8,16 @@ public class EmployeeDTO
     public long Id { get; set; }
 
     [Required]
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = string.Empty;
 
     [Required]
-    public string LastName { get; set; }
+    public string LastName { get; set; } = string.Empty;
 
     public string PhoneNumber { get; set; } = string.Empty;
 
     [Required]
-    public Campus Campus { get; set; }
+    public Campus Campus { get; set; } = null!;
+
+    [Required]
+    public Department Department { get; set; } = null!;
 }
