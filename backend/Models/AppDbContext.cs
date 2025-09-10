428ef8787436dff3f4e2fa28117e4aa8a744c02a
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2025-09-09 21:43:39 -0400

    Add all models to database context

--- backend/Models/AppDbContext.cs
+++ backend/Models/AppDbContext.cs
@@ -7,6 +7,28 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions options)
         : base(options)
     { }
-    
+
     public DbSet<Employee> Employees { get; set; }
+
+    public DbSet<Campus> Campuses { get; set; }
+
+    public DbSet<Building> Buildings { get; set; }
+
+    public DbSet<Location> Locations { get; set; }
+
+    public DbSet<Department> Departments { get; set; }
+
+    public DbSet<JobType> JobTypes { get; set; }
+
+    public DbSet<Equipment> Equipment { get; set; }
+
+    public DbSet<Company> Companies { get; set; }
+
+    public DbSet<WorkOrder> WorkOrders { get; set; }
+
+    public DbSet<WorkAction> WorkActions { get; set; }
+
+    public DbSet<SupplyOrder> SupplyOrders { get; set; }
+
+    public DbSet<Attachment> Attachments { get; set; }
 }
