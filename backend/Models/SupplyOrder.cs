d7a2d9839b6c35f94891927f8c4f3bacb547f3fb
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2025-09-09 20:48:58 -0400

    Implement supply order model

---
+++ backend/Models/SupplyOrder.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkSense.Backend.Models;
+
+public class SupplyOrder
+{
+    [Key]
+    public long Id { get; set; }
+
+    public Decimal Cost { get; set; } = 0.00m;
+
+    public string PartName { get; set; } = string.Empty;
+
+    [Required]
+    public WorkAction WorkAction { get; set; }
+}
