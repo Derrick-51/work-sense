ede653df995509ae0514b83c122940cc7d454015
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2025-09-09 21:42:47 -0400

    Update employee model

--- backend/Models/Employee.cs
+++ backend/Models/Employee.cs
@@ -1,11 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WorkSense.Backend.Models;
 
 public class Employee
 {
+    [Key]
     public long Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    // TODO: Add regular expression validator [RegularExpression(@"")]
     public string PhoneNumber { get; set; } = string.Empty;
+
+    [Required]
+    public Campus Campus { get; set; }
+
+    [Required]
+    public Department Department { get; set; }
 
     public void UpdateWithDTO(EmployeeDTO employeeDTO)
     {
--- backend/Models/EmployeeDTO.cs
+++ backend/Models/EmployeeDTO.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WorkSense.Backend.Models;
 
 public class EmployeeDTO
 {
+    [Key]
     public long Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
     public string PhoneNumber { get; set; } = string.Empty;
+
+    [Required]
+    public Campus Campus { get; set; }
 }
