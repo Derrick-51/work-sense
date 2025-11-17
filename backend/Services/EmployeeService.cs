2e5fb75e9005ae6f0b9b322c69c1fe8a9004eed8
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2025-11-16 23:14:44 -0500

    Implement employee post service

--- backend/Services/EmployeeService.cs
+++ backend/Services/EmployeeService.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using WorkSense.Backend.Models;
+using WorkSense.Backend.Services.Results;
 
 namespace WorkSense.Backend.Services;
 
@@ -11,4 +13,19 @@ public class EmployeeService : CRUDService<Employee, long>
     //
     // Extension methods beyond basic CRUD services
     //
+
+    async override public Task<ServiceResult<Employee>> Post(Employee employee)
+    {
+        // Verify foreign entities exist
+        if (!await DbContext.Campuses.AnyAsync(c => c.Key == employee.CampusKey))
+            return ServiceResult<Employee>
+            .Failure(ResultError.BadRequest,
+            $"Campus with key: {employee.CampusKey} does not exist");
+        if (!await DbContext.Departments.AnyAsync(d => d.Key == employee.DepartmentKey))
+            return ServiceResult<Employee>
+            .Failure(ResultError.BadRequest,
+            $"Department with key: {employee.DepartmentKey} does not exist");
+
+        return await base.Post(employee);
+    }
 }
