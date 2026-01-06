4276b858c4b1d341744fa0fad3aa4739d632469b
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2026-01-05 23:12:20 -0500

    Implement Building DTO

---
+++ backend/Models/DTOs/BuildingDTO.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkSense.Backend.Models;
+
+public class BuildingDTO : ITransferObject<Building, long, BuildingDTO>
+{
+    [Key]
+    public long Key { get; set; }
+
+    [Required]
+    public long CampusKey { get; set; }
+
+    public BuildingDTO() {}
+
+    public BuildingDTO(Building building)
+    {
+        Key = building.Key;
+        CampusKey = building.CampusKey;
+    }
+
+    public static implicit operator Building(BuildingDTO buildingDTO)
+    {
+        Building building = new Building();
+        building.Key = buildingDTO.Key;
+        building.CampusKey = buildingDTO.CampusKey;
+        return building;
+    }
+
+    public static BuildingDTO CreateWithEntity(Building building)
+    {
+        return new BuildingDTO(building);
+    }
+
+    public void CopyFieldsTo(Building building)
+    {
+        building.Key = Key;
+        building.CampusKey = CampusKey;
+    }
+}
